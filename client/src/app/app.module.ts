974d1daa9edec7d2a541ee23e807f96ac5fbdc5a
KillerWhale1777 <yashshinde0770@gmail.com>
2022-04-26 12:07:08 +0530

    Errors:End of Section 7

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,9 +12,12 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
 import { AuthGuard } from './_guards/auth.guard';
 import { SharedModule } from './shared.module';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,9 @@ import { SharedModule } from './shared.module';
     MemberDetailsComponent,
     ListsComponent,
     MessagesComponent,
-    
+    TestErrorsComponent,
+    ServerErrorComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -37,7 +42,8 @@ import { SharedModule } from './shared.module';
     SharedModule,
   ],
   providers: [
-    AuthGuard
+    AuthGuard,
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true}
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,9 +22,6 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe(response => {
       this.route.navigateByUrl('/members');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     });
   }
 
